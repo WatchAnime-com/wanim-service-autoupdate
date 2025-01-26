3dce8c7dc646be310d74757b4a9367f99ec8254b
Cri <87244834+L0rdL0ther@users.noreply.github.com>
2025-01-26 12:56:15 +0300

    Update auto_update.sh

--- auto_update.sh
+++ auto_update.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
 # Docker image'ı çek
-docker pull docker.watchanim.com/config-server:latest
+docker pull criy/wanim-services:video-gateway-server-latest
+docker pull criy/wanim-services:config-server-latest
+docker pull criy/wanim-services:discovery-server-latest
+docker pull criy/wanim-services:gateway-server-latest
+docker pull criy/wanim-services:video-list-service-latest
+docker pull criy/wanim-services:user-service-latest
+docker pull criy/wanim-services:video-node-server-latest
+docker pull criy/wanim-services:video-service-latest
 
 # Buraya ek olarak yapmak istediğiniz başka işlemleri de ekleyebilirsiniz
